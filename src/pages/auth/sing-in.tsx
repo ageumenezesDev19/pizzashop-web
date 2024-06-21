73f9ac11792ab1e0c10279645d21fbe09d8accea
ageu_dev19 <ageumenezes423@gmail.com>
2024-06-20 23:01:25 -0300

    Add toast notifications using sonner

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -20,9 +21,18 @@ export function SingIn() {
   } = useForm<SingInForm>()
 
   async function handleSingIn(data: SingInForm) {
-    console.log(data)
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSingIn(data),
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas')
+    }
   }
 
   return (
