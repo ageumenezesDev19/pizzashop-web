e311e9b190ca0d1ed5c761b3c61aec7145ec3e2b
ageu_dev19 <ageumenezes423@gmail.com>
2024-06-20 22:07:04 -0300

    Done the React Hook Form config

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -1,10 +1,30 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const singInForm = z.object({
+  email: z.string().email(),
+})
+
+type SingInForm = z.infer<typeof singInForm>
+
 export function SingIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SingInForm>()
+
+  async function handleSingIn(data: SingInForm) {
+    console.log(data)
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
+
   return (
     <>
       <Helmet title="login" />
@@ -19,13 +39,13 @@ export function SingIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSingIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar painel
             </Button>
           </form>
