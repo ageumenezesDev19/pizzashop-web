202ffbace80d5f3fa13f594d2a133b71b52f6fb8
ageu_dev19 <ageumenezes423@gmail.com>
2024-07-12 17:12:43 -0300

    Order pagination is working

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
 
 import { getOrders } from '@/api/get-orders'
 import { Pagination } from '@/components/pagination'
@@ -15,10 +17,37 @@ import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
-  const { data: result } = useQuery({
-    queryKey: ['orders'],
-    queryFn: getOrders,
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get('page') ?? '1')
+
+  const {
+    data: result,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ['orders', pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   })
+
+  if (isLoading) {
+    return <div>Carregando...</div>
+  }
+
+  if (isError) {
+    return <div>Erro ao buscar pedidos.</div>
+  }
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((state) => {
+      state.set('page', (pageIndex + 1).toString())
+
+      return state
+    })
+  }
 
   return (
     <>
@@ -53,7 +82,14 @@ export function Orders() {
           </div>
         </div>
 
-        <Pagination pageIndex={0} totalCount={105} perPage={10} />
+        {result && (
+          <Pagination
+            onPageChange={handlePaginate}
+            pageIndex={result.meta.pageIndex}
+            totalCount={result.meta.totalCount}
+            perPage={result.meta.perPage}
+          />
+        )}
       </div>
     </>
   )
