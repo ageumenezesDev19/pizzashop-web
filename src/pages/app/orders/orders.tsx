dde7c7246fa5ecbc335ba9c04f95ecb0f9ad4374
ageu_dev19 <ageumenezes423@gmail.com>
2024-07-12 19:50:09 -0300

    Order filters are now working

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -19,6 +19,10 @@ import { OrderTableRow } from './order-table-row'
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
 
+  const orderId = searchParams.get('orderId')
+  const customerName = searchParams.get('customerName')
+  const status = searchParams.get('status')
+
   const pageIndex = z.coerce
     .number()
     .transform((page) => page - 1)
@@ -29,8 +33,14 @@ export function Orders() {
     isError,
     isLoading,
   } = useQuery({
-    queryKey: ['orders', pageIndex],
-    queryFn: () => getOrders({ pageIndex }),
+    queryKey: ['orders', pageIndex, orderId, customerName, status],
+    queryFn: () =>
+      getOrders({
+        pageIndex,
+        orderId,
+        customerName,
+        status: status === 'all' ? null : status,
+      }),
   })
 
   if (isLoading) {
