312267be4aa4bfa47e4de6cb285fae44d8a7d7fe
ageu_dev19 <ageumenezes423@gmail.com>
2024-07-26 16:03:00 -0300

    Fix the XAxis and YAxis bug and add new e2e test Profile editing

---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+
+test('Update profile successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill('Ignite Pizza')
+  await page.getByLabel('Descrição').fill('Another Description')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Perfil atualizado com sucesso!')
+
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Cancelar' }).click()
+
+  expect(page.getByRole('button', { name: 'Ignite Pizza' })).toBeVisible()
+
+  await page.waitForTimeout(1000) // fix a bug
+})
